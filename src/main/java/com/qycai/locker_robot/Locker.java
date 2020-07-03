eddeb6ad3ca9668a4f1ac4f1d0df4e4e7b012252
qycai <1071226064@qq.com>
2020-07-03 23:56:12 +0800

    [qunyao]should throw lockerIsFullException when save bag given S locker and S bag and locker is full

--- src/main/java/com/qycai/locker_robot/Locker.java
+++ src/main/java/com/qycai/locker_robot/Locker.java
@@ -1,10 +1,24 @@
 package com.qycai.locker_robot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Locker {
+    private int capacity;
+    private String size;
+    private Map<Ticket, Bag> record = new HashMap<>();
+
     public Locker(int capacity, String size) {
+        this.capacity = capacity;
+        this.size = size;
     }
 
-    public Ticket save(Bag bag) {
-        return new Ticket();
+    public Ticket save(Bag bag) throws LockerIsFullException {
+        if (record.size() >= capacity) {
+            throw new LockerIsFullException();
+        }
+        Ticket ticket = new Ticket();
+        record.put(ticket, bag);
+        return ticket;
     }
 }
--- src/test/java/com/qycai/locker_robot/LockerTest.java
+++ src/test/java/com/qycai/locker_robot/LockerTest.java
@@ -3,15 +3,25 @@ package com.qycai.locker_robot;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LockerTest {
     @Test
-    void should_return_ticket_when_save_bag_given_S_locker_and_S_bag() {
+    void should_return_ticket_when_save_bag_given_S_locker_and_S_bag_and_locker_is_not_full() throws LockerIsFullException {
         Bag bag = new Bag("S");
         Locker locker = new Locker(3, "S");
 
         Ticket ticket = locker.save(bag);
 
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_throw_lockerIsFullException_when_save_bag_given_S_locker_and_S_bag_and_locker_is_full() throws LockerIsFullException {
+        Bag bag = new Bag("S");
+        Locker locker = new Locker(1, "S");
+        locker.save(bag);
+
+        assertThrows(LockerIsFullException.class, () -> locker.save(new Bag("S")));
     }
 }
