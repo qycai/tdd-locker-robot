e17bf437a61e23cad610821eac95fe47cc1fa804
qycai <1071226064@qq.com>
2020-07-04 11:30:59 +0800

    [qunyao]refactor the test case: should return ticket when save bag to 1st locker given primary locker robot manager two M locker and both have capacity

--- src/test/java/com/qycai/locker_robot/PrimaryLockerRobotTest.java
+++ src/test/java/com/qycai/locker_robot/PrimaryLockerRobotTest.java
@@ -1,24 +1,27 @@
 package com.qycai.locker_robot;
 
 import com.qycai.locker_robot.exception.LockerIsFullException;
+import com.qycai.locker_robot.exception.TicketIsInvalidException;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PrimaryLockerRobotTest {
     @Test
-    void should_return_ticket_when_save_bag_to_1st_locker_given_primary_locker_robot_manager_two_M_locker_and_both_have_capacity() throws LockerIsFullException {
-        Bag bag = new Bag("M");
+    void should_return_ticket_and_save_bag_to_1st_locker_when_save_bag_given_primary_locker_robot_manager_two_M_locker_and_both_have_capacity() throws LockerIsFullException, TicketIsInvalidException {
         Locker locker1 = new Locker(2, "M");
         Locker locker2 = new Locker(3, "M");
         PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(locker1, locker2));
+        Bag savedBag = new Bag("M");
 
-        Ticket ticket = primaryLockerRobot.save(bag);
+        Ticket ticket = primaryLockerRobot.save(savedBag);
 
         assertNotNull(ticket);
-
+        Bag bag = locker1.take(ticket);
+        assertEquals(savedBag, bag);
     }
 
 }
